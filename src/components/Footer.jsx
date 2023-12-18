2f7289397e50463d750b7e3b8376dbaac5898f4a
Achraf Mansari <contact@achrafmansari.com>
2023-12-18 23:10:18 +0100

    Made the footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,24 @@ import LogoLightSVG from "../assets/images/logoLightSVG.svg";
 
 const Footer = () => {
 
-    console.log(clubSocialMediaLinks);
     return (
-        <footer className="pt-24 pb-10 w-full text-white text-lg bg-blue-950">
+        <footer className="pt-24 pb-10 px-10 w-full text-white text-lg bg-blue-950">
             <div className="footer-container mx-auto w-full max-w-7xl space-y-10">
 
-                <div className="flex justify-between items-center">
+                <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-20">
                     <div className='h-12'><img src={LogoLightSVG} className="w-full h-full"/></div>
-                    <div id="footer-social-media-icons" className="">
-                        <p><span className=" font-semibold">Email:</span> <span>contact@alkhawarizmi.club</span></p>
-                        <p><span className=" font-semibold">Address:</span> <span>contact@alkhawarizmi.club</span></p>
+                    <div className="text-center md:text-left space-y-2">
+                        <p><span className="font-bold">Email:</span> <a href="mailto:contact@alkhawarizmi.club">contact@alkhawarizmi.club</a></p>
+                        <p><span className="font-bold">Address:</span> <span>Hay Ezzaitoune BP 145, Bd 2 Mars, Khouribga 25000</span></p>
                     </div>
                 </div>
 
                 <div className="border-t border-white"></div>
 
-                <ul id="footer-social-media-icons" className="w-full flex justify-center gap-4">
+                <ul id="footer-social-media-icons" className="w-full flex justify-center gap-2">
 
                     {Object.keys(clubSocialMediaLinks).map((link, i) => (
-                        <li className="h-10 w-10 text-3xl"><a href={clubSocialMediaLinks[link]}><SocialMediaIcon pltaformName={link} /></a></li>
+                        <li className="text-3xl"><a href={clubSocialMediaLinks[link]}><SocialMediaIcon pltaformName={link} /></a></li>
                     ))}
 
                 </ul>
