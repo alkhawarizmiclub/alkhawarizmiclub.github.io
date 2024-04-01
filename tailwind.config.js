f081f70fda61ca63cbc7535710e81c1927e12a41
Achraf Mansari <contact@achrafmansari.com>
2024-04-01 19:59:23 +0000

    Updated the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'header-bg': "url('assets/images/header-bg.png')",
+        'header-bg': "url('assets/images/header-bg2.png')",
+        'header-bg-layer': "url('assets/images/header-bg-layer1.svg')",
       },
       textShadow: {
         sm: '0 0 2px var(--tw-shadow-color)',
