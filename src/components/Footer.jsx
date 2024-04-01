ba2d1162aed698fa5e5cba62a201b65d74c822c8
Achraf Mansari <contact@achrafmansari.com>
2024-04-01 20:06:55 +0000

    Updated Google Maps link and the border below it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,13 @@ const Footer = () => {
         <footer className="pt-24 pb-10 px-10 w-full text-white text-lg bg-blue-950">
             <div className="footer-container mx-auto w-full max-w-7xl space-y-10">
 
-                <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-20">
+                <div className="pb-10 flex flex-col md:flex-row justify-center md:justify-between items-center gap-20 border-b border-white">
                     <div className='h-12'><img src={LogoLightSVG} className="w-full h-full"/></div>
                     <div className="text-center md:text-left space-y-2">
                         <p><span className="font-bold">Email:</span> <a href="mailto:contact@alkhawarizmi.club">contact@alkhawarizmi.club</a></p>
-                        <p><span className="font-bold">Address:</span> <a href="https://maps.app.goo.gl/iXc33UFZXd3SwotbA">Faculté Polydisciplinaire de Khouribga, Hay Ezzaitoune, Bd 2 Mars, Khouribga, Morocco</a></p>
+                        <p><span className="font-bold">Address:</span> <a href="https://maps.app.goo.gl/eCiM22i4yS7kq9fz5" target="_blank">Faculté Polydisciplinaire de Khouribga, Hay Ezzaitoune, Bd 2 Mars, Khouribga, Morocco</a></p>
                     </div>
                 </div>
-
-                <div className="border-t border-white"></div>
 
                 <ul id="footer-social-media-icons" className="w-full flex justify-center gap-2">
 
